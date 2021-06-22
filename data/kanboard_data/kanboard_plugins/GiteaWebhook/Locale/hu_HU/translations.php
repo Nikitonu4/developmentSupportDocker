47340bb71bdac3b1fc2ce98ba731e6ce5f7850b7
Nikitonu4 <nikitonu4@gmail.com>
2021-06-22 20:36:37 +0300

    for VM

---
+++ data/kanboard_data/kanboard_plugins/GiteaWebhook/Locale/hu_HU/translations.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'Gitea webhooks' => 'Gitea webhorgok',
+    'Help on Gitea webhooks' => 'Súgó a Gitea webhorgokhoz',
+    'Gitea commit received' => 'Gitea kommit érkezett',
+    'Bind Gitea webhook events to Kanboard automatic actions' => 'Gitea webhorog események kötése a Kanboard automatikus műveleteihez',
+    'Commit made by @%s on Gitea' => '@%s által készített kommit a Gitea oldalon',
+);
