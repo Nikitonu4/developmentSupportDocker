c7a13b22082663ec86521c9969e04a22f8ad1388
Nikitonu4 <nikitonu4@gmail.com>
2021-06-23 15:21:15 +0300

    update backup.py

--- backup/backup.py
+++ backup/backup.py
@@ -1,17 +1,27 @@
 import sys
 import os
-# import shutil
+import shutil
 
 def zip():
-  # print(f"{os.path.basename(root_directory)}/data")
+  # zname = f"{root_directory}/backup/dataBackup.zip"
+  # newzip=zipfile.ZipFile(zname,'w') #создаем архив
+  # newzip.write(f"{root_directory}/data")
+
+  zip_name = f"{root_directory}/backup/dataBackup"
+  directory_name = f"{root_directory}/data"
+
+  shutil.make_archive(zip_name, 'zip', directory_name)
+
   # os.system(f"mkdir {}/data")
-  os.system(f"tar -cvzf dataBackup.tar.gz {os.path.abspath('..')}/data1")
+  # os.system(f"tar -cvzf dataBackup.tar.gz {os.path.abspath('..')}/data1")
+  # print(root_directory)
   # os.system(f"sudo rm -rf {root_directory}/data") # удаляет папку data
 
 
 def unzip(path_to_data_backup):
   os.system(f"sudo rm -rf {root_directory}/data")
-  os.system(f"tar -xvf {path_to_data_backup} -C {root_directory}/data")
+  print(path_to_data_backup)
+  shutil.unpack_archive(path_to_data_backup, f"{root_directory}/data")
 
 if __name__== "__main__":
   root_directory= os.path.abspath("..")
@@ -21,6 +31,8 @@ if __name__== "__main__":
     print('Complete! Archive is in the folder with the script!')
   elif action == 'unzip':
     archive = sys.argv[2]
-    if os.path.isfile(archive) and os.path.basename(archive).endswith(".tar.gz"):
+    if os.path.isfile(archive) and os.path.basename(archive).endswith(".zip"):
       unzip(sys.argv[2])
       print('Unzip and backup successful!')
+  else:
+    print('Error! First argument must be zip or unzip!')
