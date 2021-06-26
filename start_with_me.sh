df6ebd2ca41fb33d1dbf7ff39106ceabf686c535
Nikitonu4 <nikitonu4@gmail.com>
2021-06-26 17:54:15 +0300

    add start script

---
+++ start_with_me.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install git curl htop vim python3.8
+cd $HOME
+
+# install docker and docker-compose
+
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo usermod -aG docker ${USER}
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
+git config --global user.name "nikitonu4"
+git config --global user.email "nikitonu4@gmail.com"
+
+# su - ${USER}
